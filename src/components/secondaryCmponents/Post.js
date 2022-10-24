56fac705d2389f8f5207c0d6c3382349f1e59c2d
Your Name <lucasdosualdo@gmail.com>
2022-10-23 23:03:34 -0300

    feat[hashtags/post]: redirect to hashtags page on click in tags post

--- src/components/secondaryCmponents/Post.js
+++ src/components/secondaryCmponents/Post.js
@@ -1,5 +1,5 @@
 import { SnippetBox } from "../../styles/TimelineStyles.js";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import LikeButton from "./LikeButton.js";
 import DeletePost from "./DeletePost.js";
 import { ReactTagify } from "react-tagify";
@@ -9,6 +9,8 @@ export default function Post({ user, post, loadPosts }) {
   const likeOfTheUser = (like) => like.id === user.id;
   const isLiked = post.likedBy.some(likeOfTheUser);
   const isUser = postUser.id === user.id;
+  const navigate = useNavigate();
+
   const tagStyle = {
     color: "white",
     fontWeight: 700,
@@ -21,7 +23,11 @@ export default function Post({ user, post, loadPosts }) {
       <Link to={`/user/${postUser.id}`}>
         <h3>{postUser.name}</h3>
       </Link>
-      <ReactTagify tagStyle={tagStyle} tagClicked={(tag) => alert(tag)}>
+
+      <ReactTagify
+        tagStyle={tagStyle}
+        tagClicked={(tag) => navigate(`/hashtag/${tag.slice(1)}`)}
+      >
         <h4>{post.description}</h4>
       </ReactTagify>
 
