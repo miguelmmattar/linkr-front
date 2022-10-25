cb59998b8fe03c27965f12780a4e2edfa97b4ed3
Miguel Faria <mattar1704miguel@gmail.com>
2022-10-25 16:21:21 -0300

    feat[follows] follows and no-posts messages done

--- src/services/linkr.js
+++ src/services/linkr.js
@@ -1,6 +1,14 @@
 import axios from "axios";
 
-const BASE_URL = "https://linkr-postgres-17.herokuapp.com";
+const BASE_URL = "http://localhost:4000";
+
+function getFollows(token) {
+  return axios.get(`${BASE_URL}/follows`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
 
 function getPosts(token) {
   return axios.get(`${BASE_URL}/posts`, {
@@ -102,4 +110,5 @@ export default {
   getHashtagPosts,
   editPost,
   getSearch,
+  getFollows
 };
