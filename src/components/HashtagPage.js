b62b524401a3e44d4bf2d79673451ba157c5dad5
Allan Vieira Coelho <github@allanvieira.dev>
2022-10-27 19:51:49 -0300

    fix[HashtagPage]: edit button were enable to anyone

--- src/components/HashtagPage.js
+++ src/components/HashtagPage.js
@@ -8,100 +8,98 @@ import styled from "styled-components";
 import TrendingTopics from "./secondaryComponents/Trending";
 
 export default function HashtagPage() {
-    const { user, setUser } = useContext(UserContext);
-    const { hashtag } = useParams();
-    const [posts, setPosts] = useState([]);
-    const [load, setLoad] = useState(false);
-    const [trending, setTrending] = useState([]);
-    const navigate = useNavigate("/");
+  const { user, setUser } = useContext(UserContext);
+  const { hashtag } = useParams();
+  const [posts, setPosts] = useState([]);
+  const [load, setLoad] = useState(false);
+  const [trending, setTrending] = useState([]);
+  const navigate = useNavigate("/");
 
-    function loadPosts() {
-        const promise = services.getHashtagPosts(user.token, hashtag);
+  function loadPosts() {
+    const promise = services.getHashtagPosts(user.token, hashtag);
 
-        setLoad(true);
+    setLoad(true);
 
-        promise.then(answer => {
-            setPosts(answer.data);
-            setLoad(false);
-        });
+    promise.then((answer) => {
+      setPosts(answer.data);
+      setLoad(false);
+    });
 
-        promise.catch(answer => {
-            if (answer.response.status === 401) {
-                localStorage.clear();
-                setUser(null);
-                return (navigate("/"));
-            }
-            alert("An error occured while trying to fetch the posts, please refresh the page")
-        });
+    promise.catch((answer) => {
+      if (answer.response.status === 401) {
+        localStorage.clear();
+        setUser(null);
+        return navigate("/");
+      }
+      alert(
+        "An error occured while trying to fetch the posts, please refresh the page"
+      );
+    });
+  }
+
+  function loadTrending() {
+    const promise = services.getTrending(user.token);
+    promise.then((answer) => {
+      setTrending(answer.data);
+    });
+    promise.catch((answer) => {
+      if (answer.response.status === 401) {
+        localStorage.clear();
+        setUser(null);
+        return navigate("/");
+      }
+      alert(
+        "An error occured while trying to fetch the trending topics, please refresh the page"
+      );
+    });
+  }
+
+  useEffect(() => {
+    if (!user) {
+      return navigate("/");
     }
+    loadPosts();
+    loadTrending();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [hashtag]);
 
-    function loadTrending() {
-        const promise = services.getTrending(user.token);
-        promise.then((answer) => {
-            setTrending(answer.data);
-        });
-        promise.catch((answer) =>{
-            if (answer.response.status === 401) {
-                localStorage.clear();
-                setUser(null);
-                return (navigate("/"));
-            }
-            alert(
-                "An error occured while trying to fetch the trending topics, please refresh the page"
-            )}
-        );
-    }
+  return (
+    <Posts load={load} hasTrending={true}>
+      <HashtagInfo load={load}>
+        <h1>{`# ${hashtag}`}</h1>
+      </HashtagInfo>
 
-    useEffect(() => {
-        if (!user) {
-            return navigate("/");
-        }
-        loadPosts();
-        loadTrending();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [hashtag]);
+      {posts.length === 0 ? (
+        <h6>There are no posts yet . . .</h6>
+      ) : (
+        posts.map((post, index) => (
+          <Post key={index} user={user} post={post} loadPosts={loadPosts} />
+        ))
+      )}
 
-    return (
-        <Posts load={load} hasTrending={true}>
-            <HashtagInfo load={load}>
-                <h1>{`# ${hashtag}`}</h1>
-            </HashtagInfo>
+      <Load load={load}>
+        <img src="https://i.gifer.com/ZZ5H.gif" alt="loading" />
+        <h2>Loading</h2>
+      </Load>
 
-            {posts.length === 0 ? (
-                <h6>There are no posts yet . . .</h6>
-            ) : (
-                posts.map((post, index) => (
-                    <Post
-                        key={index}
-                        user={post.user}
-                        post={post}
-                    />
-                ))
-            )}
-
-            <Load load={load}>
-                <img src="https://i.gifer.com/ZZ5H.gif" alt="loading" />
-                <h2>Loading</h2>
-            </Load>
-
-            <Trending load={load}>
-                <h3>trending</h3>
-                <div></div>
-                {trending.map((value, index) => (
-                    <TrendingTopics key={index} hashtag={value.hashtag} />
-                ))}
-            </Trending>
-        </Posts>
-    );
+      <Trending load={load}>
+        <h3>trending</h3>
+        <div></div>
+        {trending.map((value, index) => (
+          <TrendingTopics key={index} hashtag={value.hashtag} />
+        ))}
+      </Trending>
+    </Posts>
+  );
 }
 
 const HashtagInfo = styled.span`
-    display: ${props => props.load ? 'none' : 'flex'};
-    align-items: center;
-    padding: 0 18px;
-    margin-bottom: 42px;
+  display: ${(props) => (props.load ? "none" : "flex")};
+  align-items: center;
+  padding: 0 18px;
+  margin-bottom: 42px;
 
-    h1{
-        margin: 0;
-    }
+  h1 {
+    margin: 0;
+  }
 `;
---
+++ src/components/secondaryComponents/Comment.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+export default function Comment({ isAuthor, isFollower, user, index, text }) {
+    const navigate = useNavigate();
+  return (
+    <UserAccount key={index}>
+      <img
+        src={user.url}
+        alt="Profile"
+        onClick={() => {
+          return navigate(`/user/${user.id}`);
+        }}
+      />
+      <p
+        onClick={() => {
+          return navigate(`/user/${user.id}`);
+        }}
+      >
+        {user.name}
+      </p>
+      {isFollower ? (
+        <Following>• following</Following>
+      ) : isAuthor ? (
+        <Following>• post's author</Following>
+      ) : (
+        ""
+      )}
+    </UserAccount>
+  );
+}
+
+const UserAccount = styled.div`
+  width: 100%;
+  height: 50px;
+  margin-bottom: 5px;
+  display: flex;
+  align-items: center;
+  img {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    cursor: pointer;
+  }
+  p {
+    margin-left: 15px;
+    font-family: "Lato", sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 19px;
+    line-height: 23px;
+    color: #515151;
+    cursor: pointer;
+  }
+`;
+
+const Following = styled.h2`
+  margin-left: 10px;
+  font-family: "Lato", sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 19px;
+  line-height: 23px;
+  color: #c5c5c5;
+`;
